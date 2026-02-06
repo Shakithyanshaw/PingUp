e48d1adcefb16609a641bd6b4f8891d5df04fdb5
Shakithyanshaw <jeyakumarshakithyan03@gmail.com>
2026-02-06 13:55:20 +0530

    enhance RecentMessages with previews, timestamps, and unread indicator

--- client/src/components/RecentMessages.jsx
+++ client/src/components/RecentMessages.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { dummyRecentMessagesData } from '../assets/assets';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
 
 const RecentMessages = () => {
   const [messages, setMessages] = useState([]);
@@ -18,6 +19,7 @@ const RecentMessages = () => {
       <div className="flex flex-col max-h-56 overflow-y-scroll no-scrollbar">
         {messages.map((message, index) => (
           <Link
+            to={`/messages/${message.from_user_id._id}`}
             key={index}
             className="flex items-start gap-2 py-2 hover:bg-slate-100"
           >
@@ -26,6 +28,24 @@ const RecentMessages = () => {
               alt=""
               className="w-8 h-8 rounded-full"
             />
+            <div className="w-full">
+              <div className="flex justify-between">
+                <p className="font-medium">{message.from_user_id.full_name}</p>
+                <p className="text-[10px] text-slate-400">
+                  {moment(message.createdAt).fromNow()}
+                </p>
+              </div>
+              <div className="flex justify-between">
+                <p className="text-gray-500">
+                  {message.text ? message.text : 'Media'}
+                </p>
+                {!message.seen && (
+                  <p className="bg-indigo-500 text-white w-4 h-4 flex items-center justify-center rounded-full text-[10px]">
+                    1
+                  </p>
+                )}
+              </div>
+            </div>
           </Link>
         ))}
       </div>
