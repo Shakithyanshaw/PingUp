2fd7dcb7190c4ff17978af66ad2b89e04a5f2a82
Shakithyanshaw <jeyakumarshakithyan03@gmail.com>
2026-02-06 12:18:46 +0530

    render post content with hashtags and image grid

--- client/src/components/PostCard.jsx
+++ client/src/components/PostCard.jsx
@@ -3,6 +3,10 @@ import React from 'react';
 import moment from 'moment';
 
 const PostCard = ({ post }) => {
+  const postWithHashtags = post.content.replace(
+    /(#\w+)/g,
+    '<span class="text-indigo-600">$1</span>',
+  );
   return (
     <div className="bg-white rounded-xl shadow p-4 space-y-4 w-full max-w-2xl">
       {/*User Info*/}
@@ -21,6 +25,26 @@ const PostCard = ({ post }) => {
             @{post.user.username} · {moment(post.createdAt).fromNow()}
           </div>
         </div>
+      </div>
+      {/*Content*/}
+      {post.content && (
+        <div
+          className="text-gray-800 text-sm whitespace-pre-line"
+          dangerouslySetInnerHTML={{ __html: postWithHashtags }}
+        />
+      )}
+
+      {/*Images*/}
+
+      <div className="grid grid-cols-2 gap-2">
+        {post.image_urls.map((img, index) => (
+          <img
+            src={img}
+            key={index}
+            className={`w-full h-48 object-cover rounded-lg ${post.image_urls.length === 1 && 'col-span-2 h-auto'}`}
+            alt=""
+          />
+        ))}
       </div>
     </div>
   );
