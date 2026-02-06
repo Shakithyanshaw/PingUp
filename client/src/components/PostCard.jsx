4af38298cd202958601920e4348d41bcba7f73ef
Shakithyanshaw <jeyakumarshakithyan03@gmail.com>
2026-02-06 12:44:25 +0530

    add like, comment, and share actions to PostCard

--- client/src/components/PostCard.jsx
+++ client/src/components/PostCard.jsx
@@ -1,12 +1,19 @@
-import { BadgeCheck } from 'lucide-react';
-import React from 'react';
+import { BadgeCheck, Heart, MessageCircle, Share2 } from 'lucide-react';
+import React, { useState } from 'react';
 import moment from 'moment';
+import { dummyUserData } from '../assets/assets';
 
 const PostCard = ({ post }) => {
   const postWithHashtags = post.content.replace(
     /(#\w+)/g,
     '<span class="text-indigo-600">$1</span>',
   );
+
+  const [likes, setLikes] = useState(post.likes_count);
+  const currentUser = dummyUserData;
+
+  const handleLike = async () => {};
+
   return (
     <div className="bg-white rounded-xl shadow p-4 space-y-4 w-full max-w-2xl">
       {/*User Info*/}
@@ -45,6 +52,25 @@ const PostCard = ({ post }) => {
             alt=""
           />
         ))}
+      </div>
+
+      {/*Actions*/}
+      <div className="flex items-center gap-4 text-gray-600 text-sm pt-2 border-t border-gray-300">
+        <div className="flex items-center gap-1">
+          <Heart
+            className={`w-4 h-4 cursor-pointer ${likes.includes(currentUser._id) && 'text-red-500 fill-red-500'}`}
+            onClick={handleLike}
+          />
+          <span>{likes.length}</span>
+        </div>
+        <div className="flex items-center gap-1">
+          <MessageCircle className="w-4 h-4" />
+          <span>{12}</span>
+        </div>
+        <div className="flex items-center gap-1">
+          <Share2 className="w-4 h-4" />
+          <span>{7}</span>
+        </div>
       </div>
     </div>
   );
