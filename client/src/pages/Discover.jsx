cf2d29681b802bf57e4ccc57296862d32712ed26
Shakithyanshaw <jeyakumarshakithyan03@gmail.com>
2026-02-08 00:43:50 +0530

    reate discover people page

--- client/src/pages/Discover.jsx
+++ client/src/pages/Discover.jsx
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { dummyConnectionsData } from '../assets/assets';
 
 const Discover = () => {
-  return <div></div>;
+  const [input, setInput] = useState('');
+  const [users, setUsers] = useState(dummyConnectionsData);
+  const [loading, setLoading] = useState(false);
+
+  const handleSearch = async (e) => {
+    if (e.key === 'Enter') {
+      setUsers([]);
+      setLoading(true);
+      setTimeout(() => {
+        setUsers(dummyConnectionsData);
+        setLoading(false);
+      }, 1000);
+    }
+  };
+
+  return (
+    <div className="min-h-screen bg-linear-to-b from-slate-50 to-white">
+      <div className="max-w-6xl mx-auto p-6">
+        {/*Title*/}
+        <div className="mb-8">
+          <h1 className="text-3xl font-bold text-slate-900 mb-2">
+            Discover People
+          </h1>
+          <p className="text-slate-600">
+            Connect with amazing people and grow your network{' '}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Discover;
