027794e46f8092626eabe414ac348b517c28fcdc
Shakithyanshaw <jeyakumarshakithyan03@gmail.com>
2026-02-10 10:32:25 +0530

    add create post page with user info header

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { dummyUserData } from '../assets/assets';
 
 const CreatePost = () => {
-  return <div></div>;
+  const [content, setContent] = useState('');
+  const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const user = dummyUserData;
+
+  return (
+    <div className="min-h-screen bg-linear-to-r from-slate-50 to-white">
+      <div className="max-w-6xl mx-auto p-6">
+        {/*Title*/}
+        <div className="mb-8">
+          <h1 className="text-3xl font-bold text-slate-900 mb-2">
+            Create Post
+          </h1>
+          <p className="text-slate-600">Share your thoughts with the world</p>
+        </div>
+
+        {/*Form*/}
+        <div className="max-w-xl bg-white p-4 sm:p-8 sm:pb-3 rounded-xl shadow-md space-y-4">
+          {/*Header*/}
+          <div className="flex items-center gap-3">
+            <img
+              src={user.profile_picture}
+              alt=""
+              className="w-12 h-12 rounded-full shadow"
+            />
+            <div>
+              <h2 className="font-semibold">{user.full_name}</h2>
+              <p className="text-sm text-gray-500">@{user.username}</p>
+            </div>
+          </div>
+          {/*Text Area*/}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CreatePost;
