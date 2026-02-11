11f036c82c82f687ef14a5dad8bd4e1ff5efce39
Shakithyanshaw <jeyakumarshakithyan03@gmail.com>
2026-02-12 00:58:39 +0530

    add editable profile modal with image preview

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -5,6 +5,7 @@ import Loading from '../components/Loading';
 import UserProfileInfo from '../components/UserProfileInfo';
 import PostCard from '../components/PostCard';
 import moment from 'moment';
+import ProfileModal from '../components/ProfileModal';
 
 const Profile = () => {
   const { profileId } = useParams();
@@ -100,7 +101,7 @@ const Profile = () => {
         </div>
       </div>
       {/*Edit Profile Model*/}
-      {showEdit && <p>Show profile edit</p>}
+      {showEdit && <ProfileModal setShowEdit={setShowEdit} />}
     </div>
   ) : (
     <Loading />
