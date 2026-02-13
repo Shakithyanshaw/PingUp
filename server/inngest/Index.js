b52abaf277cf24e3285198fe38c85fa52a8c41d4
Shakithyanshaw <jeyakumarshakithyan03@gmail.com>
2026-02-14 01:44:58 +0530

    add auth.js

--- server/inngest/Index.js
+++ server/inngest/Index.js
@@ -1,8 +1,6 @@
 import { Inngest } from 'inngest';
 import User from '../models/User.js';
 
-//fix
-
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: 'pingup-app' });
 
