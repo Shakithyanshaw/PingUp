dc09005cc39d69f1055f8ad58f62dc37f25145a9
Shakithyanshaw <jeyakumarshakithyan03@gmail.com>
2026-02-16 00:28:24 +0530

    add unfollow user functionality

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -143,3 +143,27 @@ export const followUser = async (req, res) => {
     res.json({ success: false, message: error.message });
   }
 };
+
+//Unfollow User
+export const unfollowUser = async (req, res) => {
+  try {
+    const { userId } = req.auth();
+    const { id } = req.body;
+
+    const user = await User.findById(userId);
+    user.following = user.following.filter(user !== id);
+    await user.save();
+
+    const toUser = await User.findById(id);
+    toUser.followers = toUser.followers.filter(user !== userId);
+    await toUser.save();
+
+    res.json({
+      success: true,
+      message: 'You are no longer following this user',
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
