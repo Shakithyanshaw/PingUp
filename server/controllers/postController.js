c6591371608efec0b05bbce4e63273ea4376066b
Shakithyanshaw <jeyakumarshakithyan03@gmail.com>
2026-02-19 03:51:58 +0530

    add backend function for get posts

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -1,6 +1,7 @@
 import fs from 'fs';
 import imagekit from '../configs/imageKit.js';
 import Post from '../models/post.js';
+import User from '../models/User.js';
 
 //Add Post
 
@@ -41,6 +42,25 @@ export const addPost = async (req, res) => {
       post_type,
     });
     res.json({ success: true, message: 'Post created successfully' });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+//Get Post
+export const getFeedPost = async (req, res) => {
+  try {
+    const { userId } = req.auth();
+    const user = await User.findById(userId);
+
+    //User connections and followings
+    const userIds = [userId, ...user.connections, ...user.following];
+    const posts = await Post.find({ user: { $in: userIds } })
+      .populate('user')
+      .sort({ createdAt: -1 });
+
+    res.json({ success: true, posts });
   } catch (error) {
     console.log(error);
     res.json({ success: false, message: error.message });
