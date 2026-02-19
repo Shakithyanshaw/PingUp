541b8db0c6e5e5c3d74e71ccc713ed8981b574c9
Shakithyanshaw <jeyakumarshakithyan03@gmail.com>
2026-02-20 00:57:36 +0530

    create backend function for add user story

---
+++ server/controllers/storyController.js
@@ -0,0 +1,36 @@
+import fs from 'fs';
+import imagekit from '../configs/imageKit.js';
+import Story from '../models/story.js';
+
+// Add User Story
+export const addUserStory = async (req, res) => {
+  try {
+    const { userId } = res.auth();
+    const { content, media_type, background_color } = req.body;
+    const media = req.file;
+    let media_url = '';
+
+    //Upload medial to imagekit
+    if (media_type == 'image' || media_type == 'video') {
+      const fileBuffer = fs.readFileSync(media.path);
+      const response = await imagekit.upload({
+        file: fileBuffer,
+        fileName: media.originalname,
+      });
+      media_url = response.url;
+    }
+    //Create story
+    const story = await Story.create({
+      user: userId,
+      content,
+      media_url,
+      media_type,
+      background_color,
+    });
+
+    res.json({ success: true });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
