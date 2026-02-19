896138a1c011ddbd26256283d834405483a2c0c3
Shakithyanshaw <jeyakumarshakithyan03@gmail.com>
2026-02-20 00:36:31 +0530

    create backend unction for get user proflles and posts

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,6 @@
 import imagekit from '../configs/imageKit.js';
 import Connection from '../models/Connections.js';
+import Post from '../models/post.js';
 import User from '../models/User.js';
 import fs from 'fs';
 
@@ -281,6 +282,23 @@ export const acceptConnectionRequest = async (req, res) => {
       success: true,
       message: 'Connection accepted successfully',
     });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// Get User Profiles
+export const getUserProfiles = async (req, res) => {
+  try {
+    const { profileId } = req.body;
+    const profile = await User.findById(profileId);
+    if (profile) {
+      return res.json({ success: false, message: 'Profile not found' });
+    }
+    const posts = await Post.find(profileId).populate('user');
+
+    res.json({ success: true, profile, posts });
   } catch (error) {
     console.log(error);
     res.json({ success: false, message: error.message });
