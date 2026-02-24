547cae3afe03d12566d9100d06d8d35cb42ce3f0
Shakithyanshaw <jeyakumarshakithyan03@gmail.com>
2026-02-24 13:15:38 +0530

    create a function to update profile using frontend

--- client/src/components/ProfileModal.jsx
+++ client/src/components/ProfileModal.jsx
@@ -1,8 +1,13 @@
 import React, { useState } from 'react';
 import { Pencil } from 'lucide-react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { updateUser } from '../features/user/userSlice';
+import { useAuth } from '@clerk/clerk-react';
+import toast from 'react-hot-toast';
 
 const ProfileModal = ({ setShowEdit }) => {
+  const disPatch = useDispatch();
+  const { getToken } = useAuth();
   const user = useSelector((state) => state.user.value);
   const [editForm, setEditForm] = useState({
     username: user.username,
@@ -15,6 +20,31 @@ const ProfileModal = ({ setShowEdit }) => {
 
   const handelSaveProfile = async (e) => {
     e.preventDefault();
+    try {
+      const userData = new FormData();
+      const {
+        full_name,
+        username,
+        bio,
+        location,
+        profile_picture,
+        cover_photo,
+      } = editForm;
+
+      userData.append('username', username);
+      userData.append('bio', bio);
+      userData.append('location', location);
+      userData.append('full_name', full_name);
+      profile_picture && userData.append('profile', profile_picture);
+      cover_photo && userData.append('cover', cover_photo);
+
+      const token = await getToken();
+      disPatch(updateUser({ userData, token }));
+
+      setShowEdit(false);
+    } catch (error) {
+      toast.error(error.message);
+    }
   };
 
   return (
@@ -25,7 +55,12 @@ const ProfileModal = ({ setShowEdit }) => {
             Edit Profile
           </h1>
 
-          <form className="space-y-4" onSubmit={handelSaveProfile}>
+          <form
+            className="space-y-4"
+            onSubmit={(e) =>
+              toast.promise(handelSaveProfile(e), { loading: `Saving...` })
+            }
+          >
             {/*Profile Picture*/}
             <div className="flex flex-col items-start gap-3">
               <label
