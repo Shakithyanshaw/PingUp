7cc5b3ab7880bbb7cc66db0a993099dc680befcf
Shakithyanshaw <jeyakumarshakithyan03@gmail.com>
2026-02-26 11:51:45 +0530

    create a function to  sent connection request using frontend

--- client/src/components/UserCard.jsx
+++ client/src/components/UserCard.jsx
@@ -1,11 +1,37 @@
 import React from 'react';
 import { MapPin, MessageCircle, Plus, UserPlus } from 'lucide-react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { useAuth } from '@clerk/clerk-react';
+import { useNavigate } from 'react-router-dom';
+import api from '../api/axios';
+import toast from 'react-hot-toast';
+import { fetchUser } from '../features/user/userSlice';
 
 const UserCard = ({ user }) => {
   const currentUser = useSelector((state) => state.user.value);
+  const disPatch = useDispatch();
+  const { getToken } = useAuth();
+  const navigate = useNavigate();
 
-  const handelFollow = async () => {};
+  const handelFollow = async () => {
+    try {
+      const { data } = await api.post(
+        '/api/user/follow',
+        { id: user._id },
+        {
+          headers: { Authorization: `Bearer ${await getToken()}` },
+        },
+      );
+      if (data.success) {
+        toast.success(data.mesage);
+        disPatch(fetchUser(await getToken()));
+      } else {
+        toast.error(data.mesage);
+      }
+    } catch (error) {
+      toast.error(error.mesage);
+    }
+  };
   const handelConnectionRequest = async () => {};
 
   return (
