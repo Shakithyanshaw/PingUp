8198a1cb1e331679d00f1545b6403d34fc41360b
Shakithyanshaw <jeyakumarshakithyan03@gmail.com>
2026-02-27 09:38:45 +0530

    a small adjusment

--- server/routes/messageRoutes.js
+++ server/routes/messageRoutes.js
@@ -9,8 +9,8 @@ import { protect } from '../middleware/auth.js';
 
 const messageRouter = express.Router();
 
-messageRouter.get('/:userId', sseController);
 messageRouter.post('/send', upload.single('image'), protect, sendMessage);
 messageRouter.post('/get', protect, getChatMessages);
+messageRouter.get('/:userId', sseController);
 
 export default messageRouter;
