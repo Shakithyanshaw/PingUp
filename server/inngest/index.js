ccbac1d078a760ea76213c3b7ba38184b521d5b7
Shakithyanshaw <jeyakumarshakithyan03@gmail.com>
2026-07-29 11:02:21 +0530

    Refactor Inngest function definitions to standardize trigger structure

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -11,8 +11,12 @@ export const inngest = new Inngest({ id: 'pingup-app' });
 //Inngest Function to save user data to a database
 
 const syncUserCreation = inngest.createFunction(
-  { id: 'sync-user-from-clerk' },
-  { event: 'clerk/user.created' },
+  {
+    id: 'sync-user-from-clerk',
+    triggers: {
+      event: 'clerk/user.created',
+    },
+  },
   async ({ event }) => {
     const { id, first_name, last_name, email_addresses, image_url } =
       event.data;
@@ -36,8 +40,12 @@ const syncUserCreation = inngest.createFunction(
 
 //Inngest Function to update user data to a database
 const syncUserUpdation = inngest.createFunction(
-  { id: 'update-user-from-clerk' },
-  { event: 'clerk/user.updated' },
+  {
+    id: 'update-user-from-clerk',
+    triggers: {
+      event: 'clerk/user.updated',
+    },
+  },
   async ({ event }) => {
     const { id, first_name, last_name, email_addresses, image_url } =
       event.data;
@@ -53,8 +61,12 @@ const syncUserUpdation = inngest.createFunction(
 
 //Inngest Function to delete user data to a database
 const syncUserDeletion = inngest.createFunction(
-  { id: 'delete-user-from-clerk' },
-  { event: 'clerk/user.deleted' },
+  {
+    id: 'delete-user-from-clerk',
+    triggers: {
+      event: 'clerk/user.deleted',
+    },
+  },
   async ({ event }) => {
     const { id } = event.data;
     await User.findByIdAndDelete(id);
@@ -63,8 +75,12 @@ const syncUserDeletion = inngest.createFunction(
 
 //Inngest Function to sent Reminder when a new connection request is added
 const sendNewConnectionRequestReminder = inngest.createFunction(
-  { id: 'send-new-connection-request-reminder' },
-  { event: 'app/connection-request' },
+  {
+    id: 'send-new-connection-request-reminder',
+    triggers: {
+      event: 'app/connection-request',
+    },
+  },
   async (event, step) => {
     const { connectionId } = event.data;
 
@@ -143,8 +159,12 @@ const sendNewConnectionRequestReminder = inngest.createFunction(
 
 //Inngest Function to delete story after 24 hours
 const deleteStory = inngest.createFunction(
-  { id: 'story-delete' },
-  { event: 'app/story.delete' },
+  {
+    id: 'story-delete',
+    triggers: {
+      event: 'app/story.delete',
+    },
+  },
   async ({ event, step }) => {
     const { storyId } = event.data;
     const in24Hours = new (Date.now() + 24 * 60 * 60 * 1000)();
@@ -158,8 +178,12 @@ const deleteStory = inngest.createFunction(
 
 //Send notification of unseen messages
 const sendNotificationOfUnseenMessages = inngest.createFunction(
-  { id: 'send-unseen-messages-notification' },
-  { cron: 'TZ=America/New_York 0 9 * * *' }, //Every day at 9 am
+  {
+    id: 'send-unseen-messages-notification',
+    triggers: {
+      cron: 'TZ=America/New_York 0 9 * * *',
+    },
+  }, //Every day at 9 am
   async ({ step }) => {
     const messages = await Message.find({ seen: false }).populate('to_user_id');
     const unseenCount = {};
